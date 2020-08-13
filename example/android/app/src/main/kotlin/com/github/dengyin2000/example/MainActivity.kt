f724fe448eb21741798cbb6a6a3d58d1e03c639f
denny.deng <denny.deng@vipshop.com>
2020-08-13 17:59:57 +0800

    ## Upgrading pre 1.12 Android projects, https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects

--- example/android/app/src/main/kotlin/com/github/dengyin2000/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/github/dengyin2000/example/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.github.dengyin2000.example
 
-import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+
 }
