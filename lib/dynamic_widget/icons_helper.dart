2f147ddc89bcccaf205b16a628a796ac045145ef
Gena E <g.evstratov@gmail.com>
2022-02-22 20:42:52 +0300

    removed Icons.pie_chart_outlined

--- lib/dynamic_widget/icons_helper.dart
+++ lib/dynamic_widget/icons_helper.dart
@@ -739,7 +739,6 @@ const Map<String, IconData> IconsMap = <String, IconData>{
   'picture_in_picture': Icons.picture_in_picture,
   'picture_in_picture_alt': Icons.picture_in_picture_alt,
   'pie_chart': Icons.pie_chart,
-  'pie_chart_outlined': Icons.pie_chart_outlined,
   'pin_drop': Icons.pin_drop,
   'place': Icons.place,
   'play_arrow': Icons.play_arrow,
