23bba8a9f11b0d59c870b7d7fe96f0a71c808a12
Suryapratap Rathore <suryapratap.s.rathore@gmail.com>
2022-07-05 12:20:27 +0530

    added flex parser

---
+++ lib/dynamic_widget/basic/flex_widget_parser.dart
@@ -0,0 +1,68 @@
+import 'package:dynamic_widget/dynamic_widget.dart';
+import 'package:dynamic_widget/dynamic_widget/utils.dart';
+import 'package:flutter/widgets.dart';
+
+class FlexWidgetParser extends WidgetParser {
+  @override
+  Widget parse(Map<String, dynamic> map, BuildContext buildContext,
+      ClickListener? listener) {
+    return Flex(
+      direction: map.containsKey("direction")
+          ? parseAxis(map["direction"])
+          : Axis.horizontal,
+      crossAxisAlignment: map.containsKey('crossAxisAlignment')
+          ? parseCrossAxisAlignment(map['crossAxisAlignment'])
+          : CrossAxisAlignment.center,
+      mainAxisAlignment: map.containsKey('mainAxisAlignment')
+          ? parseMainAxisAlignment(map['mainAxisAlignment'])
+          : MainAxisAlignment.start,
+      mainAxisSize: map.containsKey('mainAxisSize')
+          ? parseMainAxisSize(map['mainAxisSize'])
+          : MainAxisSize.max,
+      textBaseline: map.containsKey('textBaseline')
+          ? parseTextBaseline(map['textBaseline'])
+          : null,
+      textDirection: map.containsKey('textDirection')
+          ? parseTextDirection(map['textDirection'])
+          : null,
+      verticalDirection: map.containsKey('verticalDirection')
+          ? parseVerticalDirection(map['verticalDirection'])
+          : VerticalDirection.down,
+      children: DynamicWidgetBuilder.buildWidgets(
+          map['children'], buildContext, listener),
+    );
+  }
+
+  @override
+  String get widgetName => "Flex";
+
+  @override
+  Map<String, dynamic> export(Widget? widget, BuildContext? buildContext) {
+    var realWidget = widget as Flex;
+    return <String, dynamic>{
+      "type": "Flex",
+      "direction": realWidget.direction == Axis.horizontal ? "horizontal" : "vertical",
+      "crossAxisAlignment":
+          exportCrossAxisAlignment(realWidget.crossAxisAlignment),
+      "mainAxisAlignment":
+          exportMainAxisAlignment(realWidget.mainAxisAlignment),
+      "mainAxisSize":
+          realWidget.mainAxisSize == MainAxisSize.max ? "max" : "min",
+      "textBaseline": realWidget.textBaseline == TextBaseline.alphabetic
+          ? "alphabetic"
+          : "ideographic",
+      "textDirection": realWidget.textDirection != null
+          ? exportTextDirection(realWidget.textDirection)
+          : null,
+      "verticalDirection":
+          realWidget.verticalDirection == VerticalDirection.down
+              ? "down"
+              : "up",
+      "children":
+          DynamicWidgetBuilder.exportWidgets(realWidget.children, buildContext),
+    };
+  }
+
+  @override
+  Type get widgetType => Flex;
+}
